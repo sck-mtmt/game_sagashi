8058b2e5f6f70cb58d0c236a063ed4a0b0d62480
sck-mtmt <108612924+sck-mtmt@users.noreply.github.com>
2022-11-16 08:47:39 +0000

    ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-
 devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    root to:"homes#top"
+  end
+  namespace :public do
+  end
+    root to:'public/homes#top'
 end
