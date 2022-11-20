caa83d063008fa40164704b557a4f4fb4c671b44
sck-mtmt <108612924+sck-mtmt@users.noreply.github.com>
2022-11-20 09:44:52 +0000

    ユーザー詳細

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   #has_many :games, dependent: :destroy
   belongs_to :review
-  enum gender: { man: 0, woman: 1, unanswered: git2}
+  enum gender: { man: 0, woman: 1, unanswered: 2}
 end
