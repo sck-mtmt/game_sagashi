61226527028c8f87470c017941cb55d82454406a
sck-mtmt <108612924+sck-mtmt@users.noreply.github.com>
2022-12-15 08:19:23 +0000

    絞り込み機能

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,9 +2,9 @@ class Public::PostCommentsController < ApplicationController
   def create
     @review = Review.find(params[:review_id])
     @post_comment = current_user.post_comments.new(post_comment_params)
-    @post_comment.review_id = review.id
+    @post_comment.review_id = @review.id
     @post_comment.save
-    redirect_to public_post_comments_path(review.id)
+    redirect_to public_review_path(@review.id)
   end
   def destroy
     PostComment.find(params[:id]).destroy
@@ -14,7 +14,7 @@ class Public::PostCommentsController < ApplicationController
   private
 
   def post_comment_params
-    params.require(:post_comment).permit(:comment)
+    params.require(:post_comment).permit(:comment, :user_id, :review_id)
   end
 
 end
