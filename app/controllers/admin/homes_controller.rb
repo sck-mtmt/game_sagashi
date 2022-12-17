284f4ddabded947fe536d6d602afc816d0441105
sck-mtmt <108612924+sck-mtmt@users.noreply.github.com>
2022-12-17 11:11:44 +0000

    管理者レビュー

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,4 +2,15 @@ class Admin::HomesController < ApplicationController
   def top
     @reviews = Review.all
   end
+
+  def destroy
+    @review = Review.find(params[:id])
+    review.destroy
+    redirect_to admin_root_path
+  end
+
+    private
+  def review_params
+    params.require(:review).permit(:user_id, :game_id, :star, :review_title, :review_body)
+  end
 end
--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,10 +1,17 @@
 class Admin::ReviewsController < ApplicationController
   def show
+    @review = Review.find(params[:id])
+    @user = @review.user
+    @game = @review.game
   end
-
-  def edit
-  end
-
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to admin_root_path
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:user_id, :game_id, :star, :review_title, :review_body)
   end
 end
